61c4fce87379ddc961f9ff1342ee60cc86d9284b
Socyno Tsai <Socyno Tsai>
2020-06-26 20:59:21 +0800

    修正镜像中的H2数据库文件错误

--- docker/start.sh
+++ docker/start.sh
@@ -4,5 +4,5 @@ export TOMCATDIR=/usr/local/tomcat/ && \
 	export H2BINDIR=/opt/h2/bin && \
 	export H2TMPLDIR=/opt/h2tmpl/h2/bin && \
 	mkdir -p "$H2BINDIR" && cd "$H2BINDIR" && \
-	if [ ! -f 'socyno-webfwk.trace.db' ]; then cp -fr "$H2TMPLDIR"/* ./ ; fi && \
+	if [ ! -f 'socyno-webfwk.mv.db' ]; then cp -fr "$H2TMPLDIR"/* ./ ; fi && \
     bash "./h2.sh" && sleep 10 && cd "$TOMCATDIR/bin" && bash ./catalina.sh run
---
+++ start.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+mkdir -p /opt/h2 && chown -R 1:1 /opt/h2 \
+    && mkdir -p /opt/log/tomcat \
+    && mkdir -p /opt/log/webfwk \
+    && chown -R 1:1 /opt/log \
+    && docker run --name webfwk -p 80:8080 -d -v /opt/h2:/opt/h2 \
+          -v /opt/log/tomcat:/usr/local/tomcat/logs \
+          -v /opt/log/webfwk:/var/log/socyno-webfwk-app \
+          -u 1 --rm socyno.org/webfwk
