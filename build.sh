df4a5259ee135900465d9f19b95eca77ceecdd0e
Socyno Tsai <Socyno Tsai>
2020-06-26 18:00:53 +0800

    修正build.sh脚本错误`

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 cd "$(dirname $0)" && \
     export WORKDIR=$(pwd) && \
-    export WORKSPACE=/opt/workspace && \
+    export WORKSPACE="/opt/workspace" && \
     export MVNSETTINGS="$WORKSPACE/maven-settigs.xml" && \
-    rm -fr "$WORKSPACE" && mkdir -p "$WORKSPACE" && cd "$WORKSPACE" && \
+    mkdir -p "$WORKSPACE" && cd "$WORKSPACE" && \
     echo '<?xml version="1.0" encoding="UTF-8"?>' >"$MVNSETTINGS" && \
     echo '<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"' >>"$MVNSETTINGS" && \
     echo '          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"' >>"$MVNSETTINGS" && \
@@ -21,38 +21,38 @@ cd "$(dirname $0)" && \
     echo '</settings>' >>"$MVNSETTINGS" && \
     mkdir -p "$WORKSPACE/socyno-jjschema" && cd "$WORKSPACE/socyno-jjschema" && \
         git init . && \
-        { git remote remote origin; git remote add origin https://github.com/socyno/socyno-jjschema.git; } \
+        { git remote remove origin; git remote add origin https://github.com/socyno/socyno-jjschema.git; } && \
         git fetch origin && \
-        git reset --hard HEAD && git clean -dfx && git checkout -B master origin/master && \
+        git reset --hard FETCH_HEAD && git clean -dfx && git checkout -B master origin/master && \
         mvn -s "$MVNSETTINGS" clean install && \
     mkdir -p "$WORKSPACE/socyno-baseutils" && cd "$WORKSPACE/socyno-baseutils" && \
         git init . && \
-        { git remote remote origin; git remote add origin https://github.com/socyno/socyno-baseutils.git; } \
+        { git remote remove origin; git remote add origin https://github.com/socyno/socyno-baseutils.git; } && \
         git fetch origin && \
-        git reset --hard HEAD && git clean -dfx && git checkout -B master origin/master && \
+        git reset --hard FETCH_HEAD && git clean -dfx && git checkout -B master origin/master && \
         mvn -s "$MVNSETTINGS" clean install && \
     mkdir -p "$WORKSPACE/socyno-webutils" && cd "$WORKSPACE/socyno-webutils" && \
         git init . && \
-        { git remote remote origin; git remote add origin https://github.com/socyno/socyno-webutils.git; } \
+        { git remote remove origin; git remote add origin https://github.com/socyno/socyno-webutils.git; } && \
         git fetch origin && \
-        git reset --hard HEAD && git clean -dfx && git checkout -B master origin/master && \
+        git reset --hard FETCH_HEAD && git clean -dfx && git checkout -B master origin/master && \
         mvn -s "$MVNSETTINGS" clean install && \
     mkdir -p "$WORKSPACE/socyno-statemachine" && cd "$WORKSPACE/socyno-statemachine" && \
         git init . && \
-        { git remote remote origin; git remote add origin https://github.com/socyno/socyno-statemachine.git; } \
+        { git remote remove origin; git remote add origin https://github.com/socyno/socyno-statemachine.git; } && \
         git fetch origin && \
-        git reset --hard HEAD && git clean -dfx && git checkout -B master origin/master && \
+        git reset --hard FETCH_HEAD && git clean -dfx && git checkout -B master origin/master && \
         mvn -s "$MVNSETTINGS" clean install && \
     mkdir -p "$WORKSPACE/socyno-webfwk" && cd "$WORKSPACE/socyno-webfwk" && \
         git init . && \
-        { git remote remote origin; git remote add origin https://github.com/socyno/socyno-webfwk.git; } \
+        { git remote remove origin; git remote add origin https://github.com/socyno/socyno-webfwk.git; } && \
         git fetch origin && \
-        git reset --hard HEAD && git clean -dfx && git checkout -B master origin/master && \
+        git reset --hard FETCH_HEAD && git clean -dfx && git checkout -B master origin/master && \
         mvn -s "$MVNSETTINGS" clean install && \
     mkdir -p "$WORKSPACE/socyno-webfwk-app" && cd "$WORKSPACE/socyno-webfwk-app" && \
         git init . && \
-        { git remote remote origin; git remote add origin https://github.com/socyno/socyno-webfwk-app.git; } \
+        { git remote remove origin; git remote add origin https://github.com/socyno/socyno-webfwk-app.git; } && \
         git fetch origin && \
-        git reset --hard HEAD && git clean -dfx && git checkout -B master origin/master && \
+        git reset --hard FETCH_HEAD && git clean -dfx && git checkout -B master origin/master && \
         mvn -s "$MVNSETTINGS" clean install && \
     cp -f "targat/socyno-webfwk*.war" "$WORKDIR/webfwk.war"
