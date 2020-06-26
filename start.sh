f011c3f805739067feb64e38ef2ae38b63c2b8aa
Socyno Tsai <Socyno Tsai>
2020-06-27 01:16:26 +0800

    完善启动脚本，添加nginx容器自启动

--- start.sh
+++ start.sh
@@ -4,7 +4,9 @@ mkdir -p /opt/h2 && chown -R 1:1 /opt/h2 \
     && mkdir -p /opt/log/tomcat \
     && mkdir -p /opt/log/webfwk \
     && chown -R 1:1 /opt/log \
-    && docker run --name webfwk -p 8080:8080 -d -v /opt/h2:/opt/h2 \
+    && docker run --name webfwk-app -p 8080:8080 -d -v /opt/h2:/opt/h2 \
           -v /opt/log/tomcat:/usr/local/tomcat/logs \
           -v /opt/log/webfwk:/var/log/socyno-webfwk-app \
-          -u 1 --rm socyno.org/webfwk
+          -u 1 --rm socyno.org/webfwk \
+    && docker run --name webfwk-static -p 80:80 \
+          --rm socyno.org/webfwk-static
