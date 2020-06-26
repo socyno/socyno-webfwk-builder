67b50144d20474da8fdd836bc2f1e988bb8124c6
Socyno Tsai <Socyno Tsai>
2020-06-27 01:12:46 +0800

    修正后端服务的端口，通过nginx代理访问

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ mkdir -p /opt/h2 && chown -R 1:1 /opt/h2 \
     && mkdir -p /opt/log/tomcat \
     && mkdir -p /opt/log/webfwk \
     && chown -R 1:1 /opt/log \
-    && docker run --name webfwk -p 80:8080 -d -v /opt/h2:/opt/h2 \
+    && docker run --name webfwk -p 8080:8080 -d -v /opt/h2:/opt/h2 \
           -v /opt/log/tomcat:/usr/local/tomcat/logs \
           -v /opt/log/webfwk:/var/log/socyno-webfwk-app \
           -u 1 --rm socyno.org/webfwk
