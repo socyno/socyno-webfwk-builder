a1f48b19d0b2f1ea1d639f71708e2e50c2f2b4d6
Socyno Tsai <Socyno Tsai>
2020-06-26 18:50:13 +0800

    修正docker内构建失败的缺陷

--- build-using-docker.sh
+++ build-using-docker.sh
@@ -2,8 +2,8 @@
 
 cd "$(dirname $0)" && \
     export WORKDIR="$(pwd)" && \
-	docker run --rm --it \
-	    -v "/opt/workspace:/opt/workspace" \
-	    -v "$(pwd):/opt/build" \
-	    codenvy/jdk8_maven3_tomcat8 \
-	    bash -c "bash /opt/build/build.sh"
+    docker run --rm -it -u root \
+        -v "$(pwd):/opt/build" \
+        -v "/opt/workspace:/opt/workspace" \
+        codenvy/jdk8_maven3_tomcat8 \
+        bash -c "bash /opt/build/build.sh"
--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-export TOMCATDIR=/home/user/tomcat8 && \
+export TOMCATDIR=/usr/local/tomcat/ && \
 	export H2BINDIR=/opt/h2/bin && \
 	export H2TMPLDIR=/opt/h2tmpl/bin && \
     cd "$H2BINDIR" && if [ ! -d 'socyno-webfwk.trace.db' ]; then cp -fr "$H2TMPLDIR"/* "$H2BINDIR/"; fi && \
